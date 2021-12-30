f7f22725f101b1b4152aa3298ee51a05c9290cb1
busraozdemr <48418589+busraozdemr@users.noreply.github.com>
2021-12-30 11:32:13 +0300

    new version at Karate

--- karateFrameworkDetailLessons/src/test/java/TestRunner.java
+++ karateFrameworkDetailLessons/src/test/java/TestRunner.java
@@ -1,38 +1,42 @@
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit4.Karate;
-import net.masterthought.cucumber.Configuration;
-import net.masterthought.cucumber.ReportBuilder;
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-@KarateOptions(tags = {"~@ignore"},
-        features = "classpath:")
+//@KarateOptions(tags = {"~@ignore"},
+//        features = "classpath:")
 public class TestRunner {
     @Test
     public void testParallel() {
-        Results results = Runner.parallel(getClass(), 1);
+        Results results = Runner.path("classpath:features")
+                .tags("~@ignore")
+                .outputCucumberJson(true)
+                .karateEnv("karate Lessons")
+                .parallel(5);
         generateReport(results.getReportDir());
-        // System.out.println("Directory---- "+results.getReportDir());
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
-        // generateReport("target/surefire-reports");
+
+
+    //   Results results = Runner.parallel(getClass(), 5);
+    //   generateReport(results.getReportDir());
+    //   // System.out.println("Directory---- "+results.getReportDir());
+       assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    //   // generateReport("target/surefire-reports");
     }
 
     public static void generateReport(String karateOutputPath) {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
         List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "demo");
+        Configuration config = new Configuration(new File("target"), "karate lessons");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
